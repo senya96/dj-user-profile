b194a0d5641d7c5a80e2e63ae869589264f423a0
senya96 <sergiynasinnuk@gmail.com>
2020-05-10 18:17:12 +0300

    set up postgres connection

--- dj_user_profile/settings.py
+++ dj_user_profile/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,10 +87,8 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
+    'default': dj_database_url.config()
 }
 
 AUTH_USER_MODEL = 'user_profile.User'
