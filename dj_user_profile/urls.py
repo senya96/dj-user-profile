be3ec0b778218580f41b1fa993d3e6adc7317a64
senya96 <sergiynasinnuk@gmail.com>
2020-05-10 18:27:27 +0300

    added static to urlpatterns

--- dj_user_profile/urls.py
+++ dj_user_profile/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from user_profile.router import router as user_profile_router
 
@@ -24,4 +26,4 @@ urlpatterns = [
     url(r'^', include(user_profile_router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
